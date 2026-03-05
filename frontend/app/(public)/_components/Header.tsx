31455e7a240298989b76c9d9b6903038ba0441bd
Bishnu Mahato <bishnukumarmahato262@gmail.com>
2026-03-05 21:08:53 +0545

    frontend

--- frontend/app/(public)/_components/Header.tsx
+++ frontend/app/(public)/_components/Header.tsx
@@ -50,7 +50,7 @@ export default function Header() {
                     </div>
 
                     <div className="flex items-center gap-2 md:justify-self-end">
-                        {/* Right: Auth + Mobile Toggle */}
+                        {/* Right: Auth + Toggle */}
                         {
                             user ? (
                                 <div className="flex items-center gap-4 md:justify-self-end">
