7d51a9acf3ae537573838622f261bd7e71980071
Bishnu Mahato <bishnukumarmahato262@gmail.com>
2026-03-05 21:14:17 +0545

    pagess

--- frontend/app/admin/layout.tsx
+++ frontend/app/admin/layout.tsx
@@ -10,9 +10,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                     <Sidebar />
                 </div>
                 <div className='w-full bg-background'>
-                    {/* Top Header  */}
+                    {/* */}
                     <Header />
-                    {/* Body Content  */}
+                    {/* Bo*/}
                     <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 p-2">
                         {children}
                     </main>
