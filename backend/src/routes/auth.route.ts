e1cbafd4932c74125d07774964741c154a1868f9
Bishnu Mahato <bishnukumarmahato262@gmail.com>
2026-03-05 20:54:32 +0545

    hee

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -7,7 +7,7 @@ const router = Router();
 
 router.post("/register", authController.register)
 router.post("/login", authController.login)
-// add remaning routes like login, logout, etc.
+
 
 
 router.get("/whoami", authorizedMiddleware, authController.getProfile);
